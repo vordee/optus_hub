54c7864e1ea179fc519a06db83923cb2df8e2fb4
vordee <vordee@gmail.com>
2026-04-02 09:57:55 -0400

    refactor(frontend): consolidate frontend styles in tailwind entry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 
 import { App } from "./app/App";
 import { ErrorBoundary } from "./app/ErrorBoundary";
-import "./styles.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
